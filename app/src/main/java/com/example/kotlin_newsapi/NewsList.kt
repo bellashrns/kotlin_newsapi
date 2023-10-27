52d3b62d91f17f85f6eb0156fd128fd47004c899
bellashrns <bella.saharani@student.umn.ac.id>
2023-10-27 19:00:04 +0700

    implement RecyclerView on Fragment

--- app/src/main/java/com/example/kotlin_newsapi/NewsList.kt
+++ app/src/main/java/com/example/kotlin_newsapi/NewsList.kt
@@ -18,63 +18,63 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 class NewsList : Fragment() {
 
-//    private val retrofit by lazy{
-//        Retrofit.Builder()
-//            .baseUrl("https://newsapi.org/v2/")
-//            .addConverterFactory(MoshiConverterFactory.create())
-//            .build()
-//    }
+    private val retrofit by lazy{
+        Retrofit.Builder()
+            .baseUrl("https://newsapi.org/v2/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
 
-//    private val newsApiService by lazy{
-//        retrofit.create(NewsApiService::class.java)
-//    }
+    private val newsApiService by lazy{
+        retrofit.create(NewsApiService::class.java)
+    }
 
-//    private val recyclerView: RecyclerView by lazy{
-//        requireView().findViewById<RecyclerView>(R.id.recycler_view)
-//    }
+    private val recyclerView: RecyclerView by lazy{
+        requireView().findViewById<RecyclerView>(R.id.recycler_view)
+    }
 
-//    private val newsAdapter by lazy {
-//        NewsAdapter(layoutInflater, GlideLoader(requireContext()))
-//    }
+    private val newsAdapter by lazy {
+        NewsAdapter(layoutInflater, GlideLoader(requireContext()))
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_news_list, container, false)
-
-//        recyclerView.adapter = newsAdapter
-//
-//        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-//
-//        getNewsResponse()
-
-        return view
+        return inflater.inflate(R.layout.fragment_news_list, container, false)
     }
 
-//    private fun getNewsResponse() {
-//        val call = newsApiService.getNews("us", "e5455e7206a647859dcb57a918281d7a")
-//        call.enqueue(object : Callback<NewsData> {
-//            override fun onResponse(call: Call<NewsData>, response: Response<NewsData>) {
-//                if (response.isSuccessful) {
-//                    val newsResponse = response.body()
-//                    val articles = newsResponse?.articles
-//
-//                    articles?.let {
-//                        newsAdapter.setData(it)
-//                    }
-//
-//                    Log.d(TAG, "Response success: $articles")
-//                } else {
-//                    Log.e(TAG, "Response failed: ${response.errorBody().toString()}")
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<NewsData>, t: Throwable) {
-//                Log.e(TAG, "Failed to get response: ", t)
-//            }
-//        })
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView.adapter = newsAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        getNewsResponse()
+    }
+
+    private fun getNewsResponse() {
+        val call = newsApiService.getNews("us", "e5455e7206a647859dcb57a918281d7a")
+        call.enqueue(object : Callback<NewsData> {
+            override fun onResponse(call: Call<NewsData>, response: Response<NewsData>) {
+                if (response.isSuccessful) {
+                    val newsResponse = response.body()
+                    val articles = newsResponse?.articles
+
+                    articles?.let {
+                        newsAdapter.setData(it)
+                    }
+
+                    Log.d(TAG, "Response success: $articles")
+                } else {
+                    Log.e(TAG, "Response failed: ${response.errorBody().toString()}")
+                }
+            }
+
+            override fun onFailure(call: Call<NewsData>, t: Throwable) {
+                Log.e(TAG, "Failed to get response: ", t)
+            }
+        })
+    }
 
     companion object {
         const val TAG = "NewsList"
